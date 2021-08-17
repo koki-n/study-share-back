af3303c55861dd872d2e86190b18dad01eabb986
koki-n <kouki19990309@gmail.com>
2021-08-17 11:30:59 +0900

    test

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
     'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
-    'exposed_headers' => ['Access-Control-Allow-Origin'],
+    'exposed_headers' => false,
     'max_age' => false,
     'supports_credentials' => false,
 ];
