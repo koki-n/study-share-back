c7bbd6b04b00405935b72a13c9d0ed93d1afdd61
koki-n <kouki19990309@gmail.com>
2021-08-19 15:12:47 +0900

    コメント

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -12,7 +12,7 @@ class CommentController extends Controller
 {
     public function show(Request $request)
     {
-        $items = post::find($request->comment);
+        $items = post::find($request->comment)->with('comment');
         return response()->json([
             'data' => $items
         ], 200);
